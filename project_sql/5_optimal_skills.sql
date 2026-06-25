bf66cd215750291e682694a238ec75731a43054c
flaaa31 <orizioflavio@gmail.com>
2026-06-25 14:09:31 +0200

    Refactor SQL query for optimal skills analysis to improve conciseness and clarity

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -7,63 +7,24 @@ after by employers but also financially rewarding, guiding professionals towards
 areas for career growth.    
 */
 
-WITH skills_demand AS (
-    SELECT
-        skills_job_dim.skill_id,
-        skills_dim.skills,
-        COUNT(skills_job_dim.job_id) AS demand_count
-    FROM job_postings_fact
-    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    WHERE 
-        job_title_short = 'Data Analyst' 
-        AND salary_year_avg IS NOT NULL
-        AND job_work_from_home = TRUE
-    GROUP BY 
-        skills_job_dim.skill_id,
-        skills_dim.skills
-),
-average_salary AS (
-    SELECT
-        skills_job_dim.skill_id,
-        ROUND(AVG(salary_year_avg), 0) AS avg_salary
-    FROM job_postings_fact
-    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    WHERE 
-        job_title_short = 'Data Analyst' 
-        AND salary_year_avg IS NOT NULL
-        AND job_work_from_home = TRUE
-    GROUP BY 
-        skills_job_dim.skill_id
-)
 
-SELECT
-    skills_demand.skill_id,
-    skills_demand.skills,
-    demand_count,
-    avg_salary
-FROM skills_demand
-INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
-WHERE demand_count > 10 -- Filter for skills that are in demand (appearing in more than 10 job postings)
-ORDER BY  avg_salary DESC, demand_count DESC
-LIMIT 25
-
--- rewriting same query concisely
 SELECT 
     skills_dim.skill_id,
     skills_dim.skills,
     COUNT(skills_job_dim.job_id) AS demand_count,
-    ROUND(AVG(salary_year_avg), 0) AS avg_salary
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE 
-    job_title_short = 'Data Analyst' 
+WHERE
+    job_title_short = 'Data Analyst'
     AND salary_year_avg IS NOT NULL
-    AND job_work_from_home = TRUE
-GROUP BY 
+    -- AND job_work_from_home = True 
+GROUP BY
     skills_dim.skill_id
-HAVING COUNT(skills_job_dim.job_id) > 10
-ORDER BY avg_salary DESC, demand_count DESC
-LIMIT 25
+HAVING
+    COUNT(skills_job_dim.job_id) > 10
+ORDER BY
+    demand_count DESC,  -- move demand_count to be first in the ORDER BY
+    avg_salary DESC 
+LIMIT 25; 
